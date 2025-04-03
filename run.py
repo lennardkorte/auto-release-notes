6c2b02307bf11e1c7e0d0564f83b8f0c3fb912b4
Lennard Korte <mail@lennardkorte.com>
2025-04-03 16:29:19 +0800

    fix

--- run.py
+++ run.py
@@ -2,14 +2,9 @@ from dotenv import load_dotenv
 from auto_release_notes import generate_release_notes
 
 def main():
-    try:
-        load_dotenv('.secrets')
-        release_notes = generate_release_notes()
-        print("\n📢 Generated Release Notes:\n")
-        print(release_notes)
-
-    except Exception as e:
-        print(f"❌ Error: {e}")
+    load_dotenv('.secrets')
+    release_notes = generate_release_notes()
+    print(release_notes)
 
 if __name__ == "__main__":
     main()
